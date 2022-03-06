adaecd855c0d66cc164133fc037814673007d3e3
lilXA1 <abedshaaban600@gmail.com>
2022-03-06 13:32:30 +0200

    .

--- src/Sections/Intro.js
+++ src/Sections/Intro.js
@@ -7,8 +7,18 @@ const Intro = () => {
       <section className="S bluebg ">
         {/* <h1 className="centertext marg0 pad0">Technology</h1> */}
         <div className="responsive">
-          <Triangle />
-          <Triangle />
+          <Triangle
+            mt="adv"
+            ci1="Empowered Finance "
+            ci2="Simplifies Work"
+            ci3="Find Relaxation And Entertainment"
+          />
+          <Triangle
+            mt="disa"
+            ci1="Uncertainty/ Insecure"
+            ci2=" Fast And More Rampant Plagiarism"
+            ci3="Increase Social Disconnect "
+          />
         </div>
       </section>
       <section className="S bluebg "></section>

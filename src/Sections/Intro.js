ff7f69ef90f8a164e6ee62a865e6848dcf5a5f21
lilXA1 <abedshaaban600@gmail.com>
2022-03-07 20:05:54 +0200

    .

--- src/Sections/Intro.js
+++ src/Sections/Intro.js
@@ -1,15 +1,16 @@
 import "./Styles/Intro.css";
 import Triangle from "./Triangle";
+import label from "../Images/label.png";
 
 const Intro = () => {
   return (
     <div className="marg0 pad0">
       {/* title */}
+
       <section className="S  ">
         <div class="title_div">
-          <span class="animate-charcter"> Technology</span>
+          <span class="animate-charcter"> Technoligy</span>
         </div>
-
         <div className="doneby">
           <span>
             by: Abed AL Ghani Shaaban, Lara Husami, Mohamad Ahmad, Rein Berro
@@ -17,7 +18,31 @@ const Intro = () => {
         </div>
       </section>
 
-      <section className="S  ">
+      {/* def / statistics / facts / quots */}
+
+      <section className="S  bluebg">
+        <ul className="textwhite list">
+          <img src={label} alt="label" className="img_label" />
+          <span className="list_span">definition</span>
+        </ul>
+
+        <ul className="textwhite list">
+          <img src={label} alt="label" className="img_label" />
+          <span className="list_span">statistics</span>
+        </ul>
+
+        <ul className="textwhite list">
+          <img src={label} alt="label" className="img_label" />
+          <span className="list_span">facts</span>
+        </ul>
+
+        <ul className="textwhite list">
+          <img src={label} alt="label" className="img_label" />
+          <span className="list_span">quots</span>
+        </ul>
+      </section>
+
+      <section className="S">
         <div className="responsive">
           <Triangle
             mt="adv"

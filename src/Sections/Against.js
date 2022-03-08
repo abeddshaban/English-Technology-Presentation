856c581888a07269e5f48a6307a6e83e616a553f
lilXA1 <abedshaaban600@gmail.com>
2022-03-08 20:49:36 +0200

    .

--- src/Sections/Against.js
+++ src/Sections/Against.js
@@ -4,30 +4,32 @@ import "./Styles/Against.css";
 const Against = () => {
   return (
     <>
-      <section className="S bluebg ">
+      <section className="S bluebg flexcol">
         <ul className="textwhite list">
           <img src={redRobot} alt="label" className="img_redRobot" />
           <span className="list_redspan">Breaches secure</span>
-          <span>selling private info on black market</span>
         </ul>
+        <span className="details">selling private info on black market</span>
       </section>
 
-      <section className="S">
+      <section className="S flexcol ">
         <ul className=" list">
           <img src={redRobot} alt="label" className="img_redRobot" />
           <span className="list_redspan">Plagiarism</span>
-          <span>
-            Answers can be found on Google, Reddit, Quora, Medium, Answerbag ..
-          </span>
         </ul>
+        <span className="details">
+          Answers can be found on Google, Reddit, Quora, Medium, Answerbag ..
+        </span>
       </section>
 
-      <section className="S bluebg ">
+      <section className="S bluebg flexcol">
         <ul className="textwhite list">
           <img src={redRobot} alt="label" className="img_redRobot" />
           <span className="list_redspan">Increases Social Disconnect</span>
-          <span></span>
         </ul>
+        <span className="details">
+          people prefer to stay on their electronics instead of other people.
+        </span>
       </section>
     </>
   );
--- src/Sections/With.js
+++ src/Sections/With.js
@@ -4,50 +4,48 @@ import "./Styles/With.css";
 const With = () => {
   return (
     <>
-      <section className="S bluebg ">
+      <section className="S bluebg flexcol">
         <ul className="textwhite list">
           <img src={blueRobot} alt="label" className="img_blueRobot" />
           <span className="list_span">Empowered Finance</span>
-
-          {/* img */}
-          <span>Trading started</span>
-          <span>form of asset like money</span>
-          <span>100% transparent</span>
         </ul>
+        {/* img */}
+        <span className="details">
+          Trading started, form of asset like money,100% transparent
+        </span>
       </section>
 
-      <section className="S">
+      <section className="S flexcol">
         <ul className=" list">
           <img src={blueRobot} alt="label" className="img_blueRobot" />
           <span className="list_span">Simplifies Work</span>
-
-          <span>
-            {/* 1-  written mail => email => video call */}
-            <table>
-              WhatsApp/Telegram/Facebook/Twitter: facilitated communications
-              -Google/Bing/Brave/Quora: access to tons of information in seconds
-              - iCloud/google drive/GitHub: Access your work from anywhere.
-            </table>
-          </span>
         </ul>
+        <span className="details">
+          {/* 1-  written mail => email => video call */}
+          <table>
+            WhatsApp/Telegram/Facebook/Twitter: facilitated communications
+            -Google/Bing/Brave/Quora: access to tons of information in seconds -
+            iCloud/google drive/GitHub: Access your work from anywhere.
+          </table>
+        </span>
       </section>
 
-      <section className="S bluebg ">
+      <section className="S bluebg flexcol">
         <ul className="textwhite list">
           <img src={blueRobot} alt="label" className="img_blueRobot" />
           <span className="list_span">
             Provides Relaxation And Entertainment
           </span>
-
-          <span>
-            <table>
-              YouTube: largest platform for entertainment. Tiktok: a new
-              platform that introduced a totally different way of serving
-              content and entertainment. Netflix: movies. Twitch: streaming.
-              Spotify: music. AppStore/Google Play: download mobile apps.
-            </table>
-          </span>
         </ul>
+
+        <span className="details">
+          <table>
+            YouTube: largest platform for entertainment. Tiktok: a new platform
+            that introduced a totally different way of serving content and
+            entertainment. Netflix: movies. Twitch: streaming. Spotify: music.
+            AppStore/Google Play: download mobile apps.
+          </table>
+        </span>
       </section>
     </>
   );
